74f9986bbea08b7fcb2ac5ee50ad978c4ed652da
Hephka <franckthevenin95@gmail.com>
2021-08-23 18:28:22 +0200

    v1

---
+++ src/pages/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <Logo />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
+        voluptate eligendi, recusandae inventore explicabo iure eaque, qui ipsum
+        incidunt aperiam quas doloribus tempore autem hic labore odit? Ducimus
+        incidunt reprehenderit nesciunt architecto, ut consectetur deleniti rem
+        dolor vero possimus voluptates cum facere suscipit ex odio amet corporis
+        itaque dolorem sunt magni, fugit eligendi distinctio. A in fugit
+        similique exercitationem nesciunt!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+        voluptatum suscipit sint. Sunt doloremque sapiente, tempore eligendi
+        error cum laudantium eum totam perferendis explicabo accusantium iure
+        dicta! Odio numquam omnis delectus fuga nam eligendi ipsa eum. Pariatur,
+        ipsam quasi velit praesentium, quis assumenda doloremque id sed
+        laudantium ducimus, recusandae aspernatur eum adipisci sunt nobis
+        commodi fuga ad dolorum unde. Earum ipsa, vero laborum sequi
+        consequuntur omnis aut soluta quisquam ducimus eos molestias dolor,
+        aliquid quis dolores? Distinctio in commodi nisi sint laborum, quia
+        sequi temporibus eius, numquam, quisquam ratione. Quibusdam voluptatem
+        omnis totam iusto? Modi aperiam esse iure iste eius nam quas in fugit
+        quaerat distinctio earum quis cumque excepturi tempora alias ut,
+        pariatur doloremque officiis accusamus facilis aspernatur maxime?
+      </p>
+    </div>
+  );
+};
+
+export default About;
